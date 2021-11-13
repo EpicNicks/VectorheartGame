db0317dfc8a8121777a35c68ffcf4b2381c02444
IanW000 <90727502+IanW000@users.noreply.github.com>
2021-11-13 18:21:03 -0500

    Updated Health Bar2

--- Assets/HowToHealthBar/Scripts/GameHandler.cs
+++ Assets/HowToHealthBar/Scripts/GameHandler.cs
@@ -13,14 +13,12 @@ public class GameHandler : MonoBehaviour {
     {
         fullHP = playerHP.HP;
         playerHP.OnPlayerHPChanged += PlayerHP_OnPlayerHPChanged;
-        
-
     }
 
     private void PlayerHP_OnPlayerHPChanged(int newHP)
     {
-        currentHPPercent = newHP / fullHP;
-        healthBar.SetSize(newHP);
+        currentHPPercent = (float)newHP / fullHP;
+        healthBar.SetSize(currentHPPercent);
     }
 
 
