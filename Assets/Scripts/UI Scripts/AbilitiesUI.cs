8cd6ff8dfe3b4a9503bc8a833efe8cc4c4250f71
IanW000 <90727502+IanW000@users.noreply.github.com>
2021-11-15 22:23:57 -0500

    Updated UI ability logic

---
+++ Assets/Scripts/UI Scripts/AbilitiesUI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilitiesUI : MonoBehaviour
+{
+    public GameObject mask;
+    private float TestTimer;
+    private float TestCoolDown;
+
+    //Test
+    //void Start()
+    //{
+    //    mask.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 120);
+    //}
+
+
+    private void Update()
+    {
+        TestTimer += Time.deltaTime;
+        TestCoolDown = TestTimer * 33f;
+        mask.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 96+ TestCoolDown);
+    }
+    public void SetSize(float sizeNormalized)
+    {
+        mask.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 40f + sizeNormalized * 0.6f);
+    }
+}
