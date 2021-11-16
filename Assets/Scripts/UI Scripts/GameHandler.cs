f828bffb033276a8c7b658f854b1ac387b324fbb
IanW000 <90727502+IanW000@users.noreply.github.com>
2021-11-16 14:22:45 -0500

    Updated Score System

--- Assets/Scripts/UI Scripts/GameHandler.cs
+++ Assets/Scripts/UI Scripts/GameHandler.cs
@@ -15,17 +15,20 @@ public class GameHandler : MonoBehaviour {
     public GameObject FailScreen;
     public TextMeshProUGUI finalScore;
 
+    public int Score;
+
     private void Start()
     {
         fullHP = playerHP.HP;
         playerHP.OnPlayerHPChanged += PlayerHP_OnPlayerHPChanged;
+        Score = 0; 
     }
     private void Update()
     {
         //here should be a function to get current score;
         //will set text as Current Score
-        countScore.text = "1";
-        finalScore.text = "1";
+        countScore.text = Score.ToString();
+        finalScore.text = Score.ToString();
     }
 
     private void PlayerHP_OnPlayerHPChanged(int newHP)
@@ -38,5 +41,13 @@ public class GameHandler : MonoBehaviour {
             FailScreen.SetActive(true);
             Time.timeScale = 0f;
         }
+    }
+
+    public int GetScore() {
+        return Score;
+    }
+    public void SetScore(int newScore)
+    {
+        Score = newScore;
     }
 }
