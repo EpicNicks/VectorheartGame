d9e94a3f37e8d04ba6fe89d8b375fbba304cae88
IanW000 <90727502+IanW000@users.noreply.github.com>
2021-11-16 14:45:22 -0500

    Update PauseMenu.cs

--- Assets/Scripts/UI Scripts/PauseMenu.cs
+++ Assets/Scripts/UI Scripts/PauseMenu.cs
@@ -8,26 +8,25 @@ public class PauseMenu : MonoBehaviour
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
     public GameObject button;
-   
-    //void Update()
-    //{
-    //    //press Escape key or click Pause button to call PauseMenu
-    // if (Input.GetKeyDown(KeyCode.Escape))
-    //    {
-    //        if (GameIsPaused)
-    //        {
-    //            button.SetActive(true);
-    //            //Cursor.visible = false;
-    //            Resume(); 
-    //        }
-    //        else
-    //        {
-    //            Pause();
-    //        }
-    //    }
 
-    //    //Cursor.visible = GameIsPaused;
-    //}
+    void Update()
+    {
+        //press Escape key or click Pause button to call PauseMenu
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                button.SetActive(true);
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+
     public void Resume()
     {
         pauseMenuUI.SetActive(false);
