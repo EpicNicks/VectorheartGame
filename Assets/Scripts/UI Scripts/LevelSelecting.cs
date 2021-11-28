86119c111b3422fe774b2b304dbcb34ac9677c7a
IanW000 <90727502+IanW000@users.noreply.github.com>
2021-11-28 02:01:32 -0500

    Done with Level Selecting Page Structure

--- Assets/Scripts/UI Scripts/LevelSelecting.cs
+++ Assets/Scripts/UI Scripts/LevelSelecting.cs
@@ -8,16 +8,16 @@ public class LevelSelecting : MonoBehaviour
 {
     public void Level1()
     {
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(1);
 
     }
     public void Level2()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
     }
     public void Level3()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(3);
     }
     public void Level4()
     {
