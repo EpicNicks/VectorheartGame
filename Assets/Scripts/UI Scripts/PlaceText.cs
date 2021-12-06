f329b60b8bbd266c55c7236fd04ebc41b86a7e81
IanW000 <90727502+IanW000@users.noreply.github.com>
2021-12-06 13:51:35 -0500

    In-game control description

---
+++ Assets/Scripts/UI Scripts/PlaceText.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceText : MonoBehaviour
+{
+    public GameObject text1;
+    public GameObject text2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(fastChangeHPcolor());
+    }
+
+    private IEnumerator fastChangeHPcolor()
+    {
+
+            text1.SetActive(true);
+            yield return new WaitForSeconds(3f);
+            text1.SetActive(false);
+            text2.SetActive(true);
+            yield return new WaitForSeconds(3f);
+            text2.SetActive(false);
+
+    }
+}
