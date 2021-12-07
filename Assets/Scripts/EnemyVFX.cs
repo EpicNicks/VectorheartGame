75ace39d5a12fe99fc36b3276f15a9b9aeef287d
EyadT <eyadayman@gmail.com>
2021-12-06 21:44:43 -0500

    ADDED  VFX AND SHADERS

---
+++ Assets/Scripts/EnemyVFX.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVFX : MonoBehaviour
+{
+
+    public GameObject normalAttack;
+    public GameObject chargedAttack;
+    public ParticleSystem spinAttack;
+    public GameObject ultimateAbility;
+    public GameObject dash;
+    public GameObject running;
+
+    private CharacterInput character;
+
+    public Transform leftFoot;
+    public Transform rightFoot;
+    public Transform feet;
+    public Transform spinLocation;
+
+    private Animator playerAnim;
+    public AudioSource playerSound;
+    public AudioClip spinSound;
+    public AudioClip dashSound;
+    public AudioClip attackSound;
+    public AudioSource enemyShootSound;
+    public AudioClip shootSound;
+    public AudioSource enemyMeleeSound;
+    public AudioClip hurtSound;
+    public AudioClip hurtSoundMelee;
+    public AudioClip enemyMeleeSwordSound;
+
+
+
+    private void Awake()
+    {
+        playerAnim = GetComponent<Animator>();
+        character = GetComponent<CharacterInput>();
+    }
+
+    public void SpinAttack()
+    {
+        spinAttack.Play();
+        playerSound.clip = spinSound;
+        playerSound.Play();
+    }
+
+    public void StartTrail()
+    {
+
+            normalAttack.SetActive(true);
+
+
+    }
+
+    public void EndTrail()
+    {
+        normalAttack.SetActive(false);
+
+    }
+
+    public void Ultimate()
+    {
+        ultimateAbility.SetActive(true);
+    }
+
+    public void StartDashTrail()
+    {
+        dash?.SetActive(true);
+        
+    }
+
+    public void EndDashTrail()
+    {
+        dash?.SetActive(false);
+    }
+
+    public void RightFootStep()
+    {
+
+        Instantiate(running, rightFoot.position, Quaternion.identity);
+    }
+
+    public void LeftFootStep()
+    {
+        Instantiate(running, leftFoot.position, Quaternion.identity);
+    }
+
+    public void attackSoundFX()
+    {
+        playerSound.clip = attackSound;
+        playerSound.Play();
+    }
+
+    public void dashSoundFX()
+    {
+        playerSound.clip = dashSound;
+        playerSound.Play();
+    }
+
+    public void enemyShootFX()
+    {
+        enemyShootSound.clip = shootSound;
+        enemyShootSound.Play();
+    }
+
+    public void enemyShootHitFX()
+    {
+        enemyShootSound.clip = hurtSound;
+        enemyShootSound.Play();
+    }
+
+    public void enemyMeleeSoundHitFX()
+    {
+        enemyMeleeSound.clip = hurtSoundMelee;
+        enemyMeleeSound.Play();
+    }
+
+    public void enemyMeleeSoundFX()
+    {
+        enemyMeleeSound.clip = enemyMeleeSwordSound;
+        enemyMeleeSound.Play();
+    }
+}
