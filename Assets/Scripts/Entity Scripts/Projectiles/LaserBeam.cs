73adbe195109d21c7667af44affab6bf0ca40a29
Nicholas Cerisano <epicnicks2.0@gmail.com>
2021-12-07 12:48:08 -0500

    added refs for audioclips in LaserBeam.cs

--- Assets/Scripts/Entity Scripts/Projectiles/LaserBeam.cs
+++ Assets/Scripts/Entity Scripts/Projectiles/LaserBeam.cs
@@ -30,6 +30,11 @@ public class LaserBeam : MonoBehaviour
     [SerializeField]
     private float speed;
 
+    [SerializeField]
+    private AudioClip bounceAudioClip;
+    [SerializeField]
+    private AudioClip onDestroyAudioClip;
+
     private void OnDrawGizmos()
     {
         Gizmos.DrawLine(transform.position, transform.position + transform.up * 3.0f);
@@ -44,31 +49,35 @@ public class LaserBeam : MonoBehaviour
         RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Time.deltaTime * speed + colRadius, collisionMask);
         if (hit.collider != null)
         {
-            if (hit.collider.CompareTag("Player"))
-            {
-                if (canHurt.HasFlag(CanHurt.PLAYER))
-                {
-                    Debug.Log("hit player");
-                    PlayerHP hp = hit.collider.GetComponent<PlayerHP>();
-                    hp.HP -= damage;
-                    Destroy(gameObject);
-                }
-            }
-            else if (hit.collider.CompareTag("Enemy"))
-            {
-                if (canHurt.HasFlag(CanHurt.ENEMY))
-                {
-                    EnemyHP hp = hit.collider.GetComponent<EnemyHP>();
-                    hp.HP -= damage;
-                    Destroy(gameObject);
-                }
-            }
+            //if (hit.collider.CompareTag("Player"))
+            //{
+            //    if (canHurt.HasFlag(CanHurt.PLAYER))
+            //    {
+            //        Debug.Log("hit player");
+            //        PlayerHP hp = hit.collider.GetComponent<PlayerHP>();
+            //        hp.HP -= damage;
+            //        Destroy(gameObject);
+            //    }
+            //}
+            //else if (hit.collider.CompareTag("Enemy"))
+            //{
+            //    if (canHurt.HasFlag(CanHurt.ENEMY))
+            //    {
+            //        EnemyHP hp = hit.collider.GetComponent<EnemyHP>();
+            //        hp.HP -= damage;
+            //        Destroy(gameObject);
+            //    }
+            //}
             if (curBounces < numDeflectionBounces)
             {
                 Vector2 reflectDir = Vector2.Reflect(ray.direction, hit.normal);
                 float rot = 90 - Mathf.Atan2(reflectDir.y, reflectDir.x) * Mathf.Rad2Deg;
                 transform.eulerAngles = new Vector3(0, 0, -rot);
                 curBounces++;
+                if (bounceAudioClip != null)
+                {
+                    AudioSource.PlayClipAtPoint(bounceAudioClip, transform.position);
+                }
             }
             else
             {
@@ -98,5 +107,13 @@ public class LaserBeam : MonoBehaviour
         transform.Translate(Vector3.up * Time.deltaTime * speed);
         CheckBounce();
         CheckHit3D();
+    }
+
+    private void OnDestroy()
+    {
+        if (onDestroyAudioClip != null)
+        {
+            AudioSource.PlayClipAtPoint(onDestroyAudioClip, transform.position);
+        }
     }
 }
